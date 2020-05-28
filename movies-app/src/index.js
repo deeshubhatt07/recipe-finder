ae4d68e6676dba4529e8e54ea9ab8e98883e29b4
deeshubhatt07 <deeshubhatt07@gmail.com>
2020-05-28 20:05:23 +0530

    Added Home

--- movies-app/src/index.js
+++ movies-app/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Home from './screens/home/home'
 
 ReactDOM.render(
-  <div></div>,
+  <Home />,
   document.getElementById('root')
 );
 
